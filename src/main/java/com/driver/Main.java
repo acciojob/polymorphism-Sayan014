159f91731413b24427b5440add06758ee6cac8c9
Sayan014 <sayanguha014@gmail.com>
2025-08-17 14:13:48 +0530

    solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,27 @@ package com.driver;
 
 public class Main {
 
+    public class Product {
+
+        public int product(int x, int y) {
+            return x * y;
+        }
+        
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+        
+        public double product(double x, double y) {
+            return x * y;
+        }
+
+        Product p = new Product();
+        
+        System.out.println("Product of 2 and 3 (int): " + p.product(2, 3));
+        System.out.println("Product of 2, 3 and 4 (int): " + p.product(2, 3, 4));
+        System.out.println("Product of 2.5 and 3.5 (double): " + p.product(2.5, 3.5));
+    }
+
+
+
 }
